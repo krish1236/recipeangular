6cab55e02750041590f685b446812c4e271a7bb3
Krishna Teja <krishnateja@Krishnas-MacBook-Pro.local>
2019-01-23 13:02:56 -0500

    second commit minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeDetailsComponent } from './recipe-book/recipe-details/recipe-details.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipeBookComponent,
+    RecipeDetailsComponent,
+    RecipeListComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
